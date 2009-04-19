670646739b8a5d476bb35be8bd13691d5b550bf4
ma.danbo <ma.danbo@65ef78d4-1fef-11de-82b5-194b17410922>
2009-04-19 20:33:32 +0000

    Building.cs class extends GeometryNode

---
+++ trunk/Building.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+using GoblinXNA;
+using GoblinXNA.Graphics;
+using GoblinXNA.SceneGraph;
+using Model = GoblinXNA.Graphics.Model;
+using GoblinXNA.Graphics.Geometry;
+using GoblinXNA.Device.Generic;
+using GoblinXNA.Physics;
+using GoblinXNA.Helpers;
+
+using GoblinXNA.UI.UI2D;
+using GoblinXNA.UI.Events;
+
+using GoblinXNA.Device.Capture;
+using GoblinXNA.Device.Vision;
+using GoblinXNA.Device.Vision.Marker;
+
+namespace Manhattanville
+{
+    class Building : GeometryNode
+    {
+        public String name;
+
+        public Building(string name) : base(name)
+        {
+            this.name = name;
+        }
+    }
+}
